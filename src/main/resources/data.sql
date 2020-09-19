983dab425b1be34268a1e8bfbf9c513c27444797
navin <navin@pop-os.localdomain>
2020-09-18 21:32:40 -0500

    db initializing with spring boot

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO category (description) VALUES ('Teaspoon');
+INSERT INTO category (description) VALUES ('Tablespoon');
+INSERT INTO category (description) VALUES ('Cup');
+INSERT INTO category (description) VALUES ('Pinch');
+INSERT INTO category (description) VALUES ('Ounce');
